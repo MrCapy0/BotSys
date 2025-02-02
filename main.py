4cddfe578a12cc62c01758979b4639cfa2d6a9d5
MrCapy0 <167662176+MrCapy0@users.noreply.github.com>
2025-02-02 17:38:50 -0400

    ...

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 profile_path = os.getenv("FIREFOX_PROFILE_DIRECTORY")
+firefox_path = os.getenv("FIREFOX_EXECUTABLE_DIRECTORY")
 
-print(profile_path)
+if firefox_path == '':
+    firefox_path = None
 
+service = webdriver.FirefoxService(executable_path=firefox_path)
 options = webdriver.FirefoxOptions()
 options.profile = webdriver.FirefoxProfile(profile_path)
 driver = webdriver.Firefox(options=options)
