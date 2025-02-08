f815c4c1322e47f477511d592b5c7ae48bedec9d
MrCapy0 <167662176+MrCapy0@users.noreply.github.com>
2025-02-08 10:43:34 -0400

    ..

--- main.py
+++ main.py
@@ -1,37 +1,20 @@
 import time
 import os
 import random
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from dotenv import load_dotenv
-from pynput.mouse import Button, Controller
 import screeninfo
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 import pyautogui
+import webbrowser
 
-
-load_dotenv()
-
-profile_path = os.getenv("FIREFOX_PROFILE_DIRECTORY")
-binary_location = os.getenv("FIREFOX_EXECUTABLE_DIRECTORY")
-
-if binary_location == '':
-    binary_location = None
-
-options = webdriver.FirefoxOptions()
-options.profile = webdriver.FirefoxProfile(profile_path)
-options.binary_location = binary_location
-driver = webdriver.Firefox(options=options)
 
 time.sleep(5)
 
-driver.get("https://open.spotify.com/collection/tracks")
+webbrowser.open("https://open.spotify.com/collection/tracks")
 
 time.sleep(5)
 
-driver.fullscreen_window()
+
+pyautogui.press('f11')
 
 screen = screeninfo.get_monitors()[0]  
 screen_width = screen.width
@@ -46,12 +29,6 @@ is_first_play = True
 time.sleep(3)
 
 while True:
-
-    if driver.current_url != 'https://open.spotify.com/collection/tracks':
-        time.sleep(4)
-
-        is_first_play = True
-        driver.get("https://open.spotify.com/collection/tracks")
 
     # if is_first_play:
     #     first_play_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".Button-sc-qlcn5g-0.iPAIAO.e-9541-button-primary.e-9541-button")))
@@ -76,7 +53,6 @@ while True:
     mouse_position_y = 0.95 * screen_height
 
     pyautogui.moveTo(mouse_position_x, mouse_position_y)
-    
 
     # mouse_position_x, mouse_position_y = 0, 0
     # mouse_position_y /= screen_height
