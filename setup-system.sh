14b59dfed85a3f09ad6c20e780b978cd38d36014
MrCapy0 <167662176+MrCapy0@users.noreply.github.com>
2025-02-08 09:59:52 -0400

    .

--- setup-system.sh
+++ setup-system.sh
@@ -1,8 +1,35 @@
 echo ""
-echo "Install system deps"
+echo "SETUP SYSTEM"
 echo ""
 
-curl -fsSL https://pyenv.run | bash
+# CURL
+echo "INSTALL CURL"
+echo ""
+
+apt update
+apt install curl
+
+# PYENV
+echo ""
+echo "INSTALL PYENV IF NOT INSTALLED AND INSTALL DEPS"
+echo ""
+
+if [ ! -d "/root/.pyenv" ]; then
+    curl -fsSL https://pyenv.run | bash
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+    echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+    echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
+    exec "$SHELL"
+fi
+
+apt update; apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl git \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+#PYTHON
+echo ""
+echo "INSTALL PYTHON AND DEPS"
+echo ""
 
 apt update
 apt install python3-pip
@@ -16,5 +43,7 @@ libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 
 
-
+echo ""
+echo "ADD PERMISSION TO RUN run.sh"
+echo ""
 chmod +x run.sh
