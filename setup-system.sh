a2b2aa59cf536af5d16ee0b59d6664e053b2aa06
MrCapy0 <167662176+MrCapy0@users.noreply.github.com>
2025-02-08 10:22:40 -0400

    ..

--- setup-system.sh
+++ setup-system.sh
@@ -2,38 +2,6 @@ echo ""
 echo "SETUP SYSTEM"
 echo ""
 
-# CURL
-echo "INSTALL CURL"
-echo ""
-
-apt update
-apt install curl
-
-# PYENV
-echo ""
-echo "INSTALL PYENV IF NOT INSTALLED AND INSTALL DEPS"
-echo ""
-
-if [ ! -d "/root/.pyenv" ]; then
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-    echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-    echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
-
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
-    echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
-    echo 'eval "$(pyenv init - bash)"' >> ~/.profile
-
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-    echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-    echo 'eval "$(pyenv init - bash)"' >> ~/.bash_profile
-
-    curl -fsSL https://pyenv.run | bash
-fi
-
-apt update; apt install build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev curl git \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
 #PYTHON
 echo ""
 echo "INSTALL PYTHON AND DEPS"
